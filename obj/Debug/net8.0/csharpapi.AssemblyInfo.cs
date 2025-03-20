e2e00a08e628cf7241742074df027b822cd7dd1d
DioGZ <jdguvaraz@gmail.com>
2025-03-19 21:22:47 -0500

    arreglo al launch.json

--- obj/Debug/net8.0/csharpapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharpapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharpapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942e98552ac6e4a2ac1c1423778e50a67b53783e")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharpapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharpapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
